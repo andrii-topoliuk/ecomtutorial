6ebc42b7797bf5d4d8760c0b741710137945c65e
Andrii Topoliuk <andrew.topolyuk@gmail.com>
2022-06-04 22:29:46 +0300

    Initial commit

---
+++ src/main/java/com/andriit/ecomtutorial/EcommerceTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.andriit.ecomtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andriit/ecomtutorial/EcommTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriit.ecomtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
